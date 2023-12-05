8d049f3d0575846ba278b34a7bcb205fb34f9926
yogitajoshi098 <yogitajoshi603@gmail.com>
2023-12-05 15:36:17 +0530

    added a function

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,15 +1,24 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import './Navbar.css';
 import {Link} from 'react-router-dom'
 import {ShoppingCart} from 'phosphor-react';
+import { ShopContext } from '../context/ShopContext';
+
+
+
 export default function Navbar() {
+
+  const {increaseCount} = useContext(ShopContext)
   return (
     <div className='navbar'>
       <div className='links'>
 <Link to='/'>Shop</Link>
+<div className='iconSpan'>
 <Link to='/cart'>
     <ShoppingCart size={32}/>
 </Link>
+<span style={{marginBottom:"3px", color:'white'}}>{increaseCount()}</span>
+</div>
       </div>
     </div>
   )
--- src/context/ShopContext.js
+++ src/context/ShopContext.js
@@ -1,54 +1,70 @@
 import React, { createContext, useState } from "react"
-import {PRODUCTS} from '../products'
+import { PRODUCTS } from '../products'
 export const ShopContext = createContext(null)
 
-const getDefaultCart =()=>{
-    let cart ={}
-        for(let i=0;i< PRODUCTS.length+1; i++){
-        cart[i] =0;
+const getDefaultCart = () => {
+    let cart = {}
+    for (let i = 0; i < PRODUCTS.length + 1; i++) {
+        cart[i] = 0;
+    }
+
+    return cart
+
+}
+
+export const ShopContextProvider = (props) => {
+    const [cartItems, setCartItems] = useState(getDefaultCart());
+
+
+    function getTotalCartAmount() {
+        let totalAmount = 0;
+        for (const item in cartItems) {
+            if (cartItems[item] > 0) {
+                let itemInfo = PRODUCTS.find((product) => product.id === Number(item));
+                totalAmount += cartItems[item] * itemInfo.price
+            }
         }
 
-        return cart
-    
-}
 
-export const ShopContextProvider = (props)=>{
-    const [cartItems, setCartItems] = useState(getDefaultCart() );
-    
 
-    function getTotalCartAmount(){
-let totalAmount = 0;
-for(const item in cartItems){
-    if(cartItems[item]>0){
-        let itemInfo= PRODUCTS.find((product)=> product.id===Number(item));
-        totalAmount+= cartItems[item] * itemInfo.price
+
+        return totalAmount;
     }
-}
 
-return totalAmount;
+
+    function increaseCount() {
+        let count = 0;
+        for (const val in cartItems) {
+            if (cartItems[val] > 0) {
+                count += cartItems[val]
+            }
+        }
+
+        return count;
     }
 
 
 
-    function addToCart(itemId){
-setCartItems((prev)=>({...prev, [itemId]: prev[itemId] +1}))
+    function addToCart(itemId) {
+        setCartItems((prev) => ({ ...prev, [itemId]: prev[itemId] + 1 }))
     }
 
-    function removeFromCart(itemId){
-        setCartItems((prev)=>({...prev, [itemId]: prev[itemId] -1}))
-            }
+    function removeFromCart(itemId) {
+        setCartItems((prev) => ({ ...prev, [itemId]: prev[itemId] - 1 }))
+    }
 
-            function updateCartItemCount(newAmount, itemId){
-                setCartItems((prev)=>({...prev, [itemId]: newAmount}))
-            }
+    function updateCartItemCount(newAmount, itemId) {
+        setCartItems((prev) => ({ ...prev, [itemId]: newAmount }))
+    }
 
-            const ContextValue = {
-                cartItems,
-                addToCart,
-                removeFromCart,
-                updateCartItemCount,
-                getTotalCartAmount
-            }
+    const ContextValue = {
+        cartItems,
+        addToCart,
+        removeFromCart,
+        updateCartItemCount,
+        getTotalCartAmount,
+        increaseCount
+    }
 
 
     return <ShopContext.Provider value={ContextValue}>
